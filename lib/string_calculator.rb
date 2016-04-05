9b60ed9057ae597ce403c54177151c11aff5089c
russellvaughan <russellvaughan@gmail.com>
2016-04-05 11:54:02 +0100

    test passes for 0, and 1 character count

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,13 @@
 class StringCalculator
 
-	def integer_add
+	def integer_add string_numbers
+		count = string_count(string_numbers)
+		count < 1 ? 0 : string_numbers.to_i
+	end
+
+	def string_count(string_numbers)
+		string_numbers.chars.count
 	end
 
 end
+
--- spec/int_add_spec.rb
+++ spec/int_add_spec.rb
@@ -2,7 +2,16 @@ describe StringCalculator do
 		let(:calculator) { described_class.new } 
 		
 		describe "#integer_add" do
-			it { is_expected.to respond_to(:integer_add).with(0).arguments }
+			it { is_expected.to respond_to(:integer_add).with(1).arguments }
+			
+			it 'will return 0 when passed an empty string' do 
+			 expect(calculator.integer_add("")).to eq(0)
+			end
+
+			it 'will return an integer when passed an string of that number' do 
+			 expect(calculator.integer_add("4")).to eq(4)
+			end
+
 		end
 
 end
